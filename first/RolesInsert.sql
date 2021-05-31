12fdeb5d4c2e40df5972b98efd856a1268920fa8
ArtemIyX <68477399+ArtemIyX@users.noreply.github.com>
2021-05-31 11:41:50 +0300

    Podorojko_hw07

---
+++ first/RolesInsert.sql
@@ -0,0 +1,12 @@
+﻿INSERT INTO ROLES (Title) VALUES 
+(N'Administrative'),
+(N'Business'),
+(N'Customer service'),
+(N'Finance'),
+(N'Information technology'),
+(N'Leadership'),
+(N'Legal'),
+(N'Marketing'),
+(N'Public relations'),
+(N'Purchasing and operations'),
+(N'Sales')
